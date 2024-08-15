32ec90cc6d81a72828d6a746e8cb4ed970c36502
Pratham <pratham200209@gmail.com>
2024-08-16 03:18:02 +0530

    fixing a small bug where user password was being sent back

--- src/api/user/user.service.ts
+++ src/api/user/user.service.ts
@@ -1,28 +1,40 @@
 import { database } from "../../db/db";
-import { InsertUser, SelectUser, usersTable } from "../../db/schemas/schema";
+import { usersTable } from "../../db/schemas/schema";
 import { eq, lt, gte, ne } from "drizzle-orm";
 
 export const getUserByPhoneNumber = async (
     phone_number: number
-): Promise<SelectUser | null> => {
+): Promise<any> => {
     // Get user from database with phone number
     const user = await database
-        .select()
+        .select({
+            id: usersTable.id,
+            name: usersTable.name,
+            username: usersTable.username,
+            phoneNumber: usersTable.phoneNumber,
+            email: usersTable.email,
+        })
         .from(usersTable)
         .where(eq(usersTable.phoneNumber, phone_number));
 
     return user.length > 0 ? user[0] : null;
 };
 
-export const getUser = async (id: number): Promise<SelectUser | null> => {
+export const getUser = async (id: number): Promise<any> => {
     // Get user from database with id
     const user = await database
-        .select()
+        .select({
+            id: usersTable.id,
+            name: usersTable.name,
+            username: usersTable.username,
+            phoneNumber: usersTable.phoneNumber,
+            email: usersTable.email,
+        })
         .from(usersTable)
         .where(eq(usersTable.id, id));
 
     return user.length > 0 ? user[0] : null;
-}
+};
 
 export const createUser = async (
     name: string,
